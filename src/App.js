8cd7896b79f5afae58338623310127d44ba1dfdd
x <fedorenkomcbook@gmail.com>
2023-10-27 17:24:37 +0200

    pushing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {Posts} from "./components/PostsContainer/Posts/Posts";
+import {Launches} from "./components/LaunchesContainer/Launches/Launches";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        {/*<Posts/>*/}
+        <Launches/>
+      </div>
   );
-}
+};
 
-export default App;
+export {App};
